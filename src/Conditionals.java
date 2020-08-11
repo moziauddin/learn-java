4627a88c867b70da3239d8d6dd4df90e90e29c62
moziauddin <moziauddin@gmail.com>
2020-08-11 16:48:39 +1000

    Working: Expressions and Error Handling

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,27 +1,28 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Conditionals {
     public static void main(String[] args) {
-        checkCondition();
+        expressions();
     }
-    private static void checkCondition() {
-        // Variable to base the condition on
-        Scanner word = new Scanner(System.in);
-        System.out.print("Enter a word: ");
-        char option = word.nextLine().toLowerCase().charAt(0);
-        switch (option) {
-            case 'p':
-                System.out.println("Pause");
-                break;
-            case 'q':
-                System.out.println("Quit");
-                break;
-            case 's':
-                System.out.println("Play");
-                break;
-            default:
-                System.out.println("no match");
-                break;
+
+    public static void expressions() {
+        Scanner scan = new Scanner(System.in);
+        try {
+            System.out.print("Enter first number: ");
+            int firstNum = scan.nextInt();
+            System.out.print("Enter second number: ");
+            int secondNum = scan.nextInt();
+            int result = sumTwoNumbers(firstNum, secondNum);
+            System.out.println("The sum is: " + result);
+        } catch (InputMismatchException e) {
+            System.out.println("Enter a proper number.");
+            System.exit(0);
         }
+    }
+
+    public static int sumTwoNumbers(int num1, int num2) {
+        int sum = num1+num2;
+        return sum;
     }
 }
