e02179d078f03b14c65f4f68d34bfb008f403b25
moziauddin <moziauddin@gmail.com>
2020-08-11 12:14:48 +1000

    Working: One class solution to access modifers

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,16 +1,13 @@
-class Conditionals {
-    void checkCondition() {
-        int num = 11;
+public class Conditionals {
+    public static void main(String[] args) {
+        checkCondition();
+    }
+    private static void checkCondition() {
+        int num = 0;
         if (num == 0) {
             System.out.println("num is 0");
         } else  {
             System.out.println("num is not zero");
         }
-    }
-}
-class Main {
-    public static void main(String[] args) {
-        Conditionals cond = new Conditionals();
-        cond.checkCondition();
     }
 }
