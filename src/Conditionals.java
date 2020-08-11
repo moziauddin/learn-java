e55c466f7b534e18c35f1ba5abf8bd59606ff566
moziauddin <moziauddin@gmail.com>
2020-08-11 16:16:36 +1000

    Working: Added switch statement to the class

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -1,13 +1,27 @@
+import java.util.Scanner;
+
 public class Conditionals {
     public static void main(String[] args) {
         checkCondition();
     }
     private static void checkCondition() {
-        int num = 0;
-        if (num == 0) {
-            System.out.println("num is 0");
-        } else  {
-            System.out.println("num is not zero");
+        // Variable to base the condition on
+        Scanner word = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        char option = word.nextLine().toLowerCase().charAt(0);
+        switch (option) {
+            case 'p':
+                System.out.println("Pause");
+                break;
+            case 'q':
+                System.out.println("Quit");
+                break;
+            case 's':
+                System.out.println("Play");
+                break;
+            default:
+                System.out.println("no match");
+                break;
         }
     }
 }
